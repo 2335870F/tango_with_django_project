81030b361ed83b0c7a00a1389a11c211d77f3a9d
“Lynda <lyndafaller@lyndas-air.home>
2019-01-18 22:50:24 +0000

    Updated 4 files as directed at the end of chapter 3 of TWD to produce first Django powered webpage.

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    #this maps the basic URL to the index view in the rango app.
+    url(r'^rango/', include ('rango.urls')),
+    #above maps any URLs starting with rango/ to be handled by the rango application
     url(r'^admin/', admin.site.urls),
 ]
